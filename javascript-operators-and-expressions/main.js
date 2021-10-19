0ef25364e9735f6798513ffb9e4cbac703d82ec1
Johnny Baltazar <baltazarjohnny@yahoo.com>
2021-10-18 21:51:06 -0700

    Completed javascript-operatos-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 50;
+var height = 75;
+var area = width * height;
+
+console.log('Value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 250;
+var payment = 45;
+var change = payment - bill;
+
+console.log('Value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 85;
+var midterm = 97;
+var final = 91;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Johnny';
+var lastName = 'Baltazar';
+var fullName = firstName + ' ' + lastName;
+
+console.log('Value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var ph = 55;
+var isAcidic = ph < 7;
+
+console.log('Value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 11;
+var isSparta = headCount === 300;
+
+console.log('Value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName + ' ' + 'is the GOAT!';
+
+console.log(motto);
+console.log('typeof motto:', typeof motto);
