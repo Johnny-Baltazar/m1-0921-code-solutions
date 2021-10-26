d50fd29a6b50529e702fdf4aba13c7d8a00c03ea
Johnny Baltazar <baltazarjohnny@yahoo.com>
2021-10-26 11:53:23 -0700

    Completed javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,67 +1,67 @@
 var orderHistory = [
   {
-    orderNumber: 113 + '-' + 2883177 + '-' + 2648248,
-    orderDate: 'July ' + 3 + ', ' + 2020,
-    total: '$' + 138 + '.' + 93,
-    deliveryDate: 'July ' + 5 + ', ' + 2020,
+    orderNumber: '113-2883177-2648248',
+    orderDate: 'July 3, 2020',
+    total: '$138.93',
+    deliveryDate: 'July 5, 2020',
     shipTo: 'JS Masher',
     productOne: [
       {
         productName: 'GameCube Controller - Super Smash Bros. Edition (Nitendo Switch)',
-        price: '$' + 94 + '.' + 95,
-        returnWindow: 'Aug ' + 4 + ', ' + 2020
+        price: '$94.95',
+        returnWindow: 'Aug 4, 2020'
       }
     ],
     productTwo: [
       {
         productName: 'The Art of Sql',
-        price: '$' + 33 + '.' + 99,
+        price: '$33.99',
         author: 'Faroult, Stephanie',
-        returnWindow: 'Aug ' + 4 + ', ' + 2020
+        returnWindow: 'Aug 4, 2020'
       }
     ]
   },
   {
-    orderNumber: 114 + '-' + 2875557 + '-' + 9059409,
-    orderDate: 'July ' + 4 + ', ' + 2020,
-    total: '$' + 17 + '.' + 22,
-    deliveryDate: 'July ' + 7 + ', ' + 2020,
+    orderNumber: '114-2875557-9059409',
+    orderDate: 'July 4, 2020',
+    total: '$17.22',
+    deliveryDate: 'July 7, 2020',
     shipTo: 'JS Masher',
     product: [
       {
         productName: 'GameCube Controller Adapter. Super Smash Bros Switch GameCube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-GameCube Adapter',
-        price: '$' + 15 + '.' + 98,
-        returnWindow: 'Aug ' + 5 + ', ' + 2020
+        price: '$15.98',
+        returnWindow: 'Aug 5, 2020'
       }
     ]
   },
   {
-    orderNumber: 113 + '-' + 9984268 + '-' + 1280257,
-    orderDate: 'July ' + 19 + ', ' + 2020,
-    total: '$' + 44 + '.' + 53,
-    deliverDate: 'July ' + 20 + ', ' + 2020,
+    orderNumber: '113-9984268-1280257',
+    orderDate: 'July 19, 2020',
+    total: '$44.53',
+    deliverDate: 'July 20, 2020',
     shipTo: 'JS Masher',
     product: [
       {
         productName: 'The Timeless Way of Building',
-        price: '$' + 41 + '.' + 33,
+        price: '$41.33',
         author: 'Alexander, Christopher',
-        returnWindow: 'Aug ' + 19 + ', ' + 2020
+        returnWindow: 'Aug 19, 2020'
       }
     ]
   },
   {
-    orderNumber: 114 + '-' + 3941689 + '-' + 8772232,
-    orderDate: 'Aug ' + 4 + ', ' + 2020,
-    total: '$' + 34 + '.00',
-    deliveryDate: 'Aug ' + 8 + ', ' + 2020,
+    orderNumber: '114-3941689-8772232',
+    orderDate: 'Aug 4, 2020',
+    total: '$34.00',
+    deliveryDate: 'Aug 8, 2020',
     shipTo: 'JS Masher',
     product: [
       {
         productName: 'JavaScript for impatient programmers',
-        price: '$' + 31 + '.' + 55,
+        price: '$31.55',
         author: 'Rauschmayer, Dr. Axel',
-        returnWindow: 'Sep ' + 7 + ', ' + 2020
+        returnWindow: 'Sep 7, 2020'
       }
     ]
   }
